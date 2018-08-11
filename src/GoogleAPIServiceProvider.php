236d2a7b1a2527c72133b902d6d17111d38b4982
Rinjax <steve@encara.co.uk>
2018-08-11 17:12:26 +0100

    first commit

---
+++ src/GoogleAPIServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rinjax\GoogleAPI;
+
+use Illuminate\Support\ServiceProvider;
+
+class GoogleAPIServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
